ed896ba36ec1d842414d642e666482583309a0fb
DiegoPertierraM <diegopmenendez@gmail.com>
2024-03-24 20:52:45 +0100

    Add individual pokemon stats with conditional colors

--- projects/pokeapp/src/app/features/poke-details/poke-details.component.ts
+++ projects/pokeapp/src/app/features/poke-details/poke-details.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { PokemonDetails } from '../../core/models/pokeModel';
 import { PokeapiRepoService } from '../../core/services/pokeapi/pokeapi.repo.service';
 import { ActivatedRoute } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLeftLong } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'isdi-poke-details',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   template: `
     @if (pokemon) {
     <ul class="poke-details">
+      <fa-icon [icon]="faLeftLong" (click)="goBack()"></fa-icon>
       <li class="poke-name">{{ pokemon.name }}</li>
       <li>Id: {{ pokemon.id }}</li>
       <li>
@@ -62,12 +66,56 @@ import { ActivatedRoute } from '@angular/router';
       </li>
       <li><span>Height:</span> {{ pokemon.height }}</li>
       <li><span>Weight:</span> {{ pokemon.weight }}</li>
+      @if (pokemon.abilities) {
       <li class="poke-abilities">
         <h2>Abilities:</h2>
         <span>{{ pokemon.abilities[0].ability.name }}</span>
-        @if (pokemon.abilities[1].ability.name) {
+        @if (pokemon.abilities.length > 1) {
         <span>{{ pokemon.abilities[1].ability.name }}</span>
         }
+      </li>
+      }
+      <br />
+      <hr />
+      <li>
+        <ul class="poke-stats">
+          <li class="poke-stat">
+            <span>{{ pokemon.stats[0].stat.name }}:</span>
+            <span [class]="chooseStatColor(pokemon.stats[0].base_stat)">{{
+              pokemon.stats[0].base_stat
+            }}</span>
+          </li>
+          <li class="poke-stat">
+            <span>{{ pokemon.stats[1].stat.name }}:</span>
+            <span [class]="chooseStatColor(pokemon.stats[1].base_stat)">
+              {{ pokemon.stats[1].base_stat }}</span
+            >
+          </li>
+          <li class="poke-stat">
+            <span>{{ pokemon.stats[2].stat.name }}:</span>
+            <span [class]="chooseStatColor(pokemon.stats[2].base_stat)">
+              {{ pokemon.stats[2].base_stat }}</span
+            >
+          </li>
+          <li class="poke-stat">
+            <span>{{ pokemon.stats[3].stat.name }}:</span>
+            <span [class]="chooseStatColor(pokemon.stats[3].base_stat)">
+              {{ pokemon.stats[3].base_stat }}</span
+            >
+          </li>
+          <li class="poke-stat">
+            <span>{{ pokemon.stats[4].stat.name }}:</span>
+            <span [class]="chooseStatColor(pokemon.stats[4].base_stat)">{{
+              pokemon.stats[4].base_stat
+            }}</span>
+          </li>
+          <li class="poke-stat">
+            <span>{{ pokemon.stats[5].stat.name }}:</span>
+            <span [class]="chooseStatColor(pokemon.stats[5].base_stat)">{{
+              pokemon.stats[5].base_stat
+            }}</span>
+          </li>
+        </ul>
       </li>
     </ul>
     }
@@ -76,10 +124,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export default class PokeDetailsComponent implements OnInit {
   pokemon: PokemonDetails | undefined;
+  faLeftLong = faLeftLong;
 
   constructor(
     private route: ActivatedRoute,
-    private pokeapiRepoService: PokeapiRepoService
+    private pokeapiRepoService: PokeapiRepoService,
+    private _location: Location
   ) {}
 
   ngOnInit(): void {
@@ -95,6 +145,10 @@ export default class PokeDetailsComponent implements OnInit {
         this.pokemon = data;
       },
     });
+  }
+
+  goBack() {
+    this._location.back();
   }
 
   chooseType(type: string): string {
@@ -139,6 +193,20 @@ export default class PokeDetailsComponent implements OnInit {
         return 'stellar';
       default:
         return '';
+    }
+  }
+
+  chooseStatColor(stat: number) {
+    if (stat < 50) {
+      return 'awful';
+    } else if (stat < 70) {
+      return 'low';
+    } else if (stat < 100) {
+      return 'mid';
+    } else if (stat < 130) {
+      return 'high';
+    } else {
+      return 'goated';
     }
   }
 }
