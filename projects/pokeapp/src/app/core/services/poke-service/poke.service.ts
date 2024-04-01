baf7bcff444c53c5562a6599c005cecd60d9b1e0
DiegoPertierraM <diegopmenendez@gmail.com>
2024-04-01 09:37:47 +0200

    Move app state to poke service

--- projects/pokeapp/src/app/core/services/poke-service/poke.service.ts
+++ projects/pokeapp/src/app/core/services/poke-service/poke.service.ts
@@ -1,34 +1,54 @@
-import { Injectable, OnInit } from '@angular/core';
-import { Pokemon } from '../../models/pokeModel';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Pokemon, PokemonBase, PokemonResponse } from '../../models/pokeModel';
+import { BehaviorSubject, forkJoin } from 'rxjs';
 import { PokeapiRepoService } from '../pokeapi/pokeapi.repo.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokeService {
-  private pokemonList: Pokemon[] = [];
-  private pokemon$ = new BehaviorSubject<Pokemon[]>([]);
+  private pokemonList$: BehaviorSubject<Pokemon[]> = new BehaviorSubject<
+    Pokemon[]
+  >([]);
 
-  constructor(private pokeSrv: PokeapiRepoService) {
-    this.initialPokemon();
+  constructor(private pokeApiRepoSrv: PokeapiRepoService) {
+    this.fetchPokemonList();
+  }
+
+  fetchPokemonList(): void {
+    this.pokeApiRepoSrv
+      .getPokemonList()
+      .subscribe((response: PokemonResponse) => {
+        const results = response.results;
+        const fetchPokemonDetails = results.map((result: PokemonBase) => {
+          return this.pokeApiRepoSrv.getPokemonDetails(result.url);
+        });
+        forkJoin(fetchPokemonDetails).subscribe((pokemonDetails: Pokemon[]) => {
+          const pokemonArray: Pokemon[] = pokemonDetails.map(
+            (detail: Pokemon) => {
+              return { ...detail };
+            }
+          );
+          this.pokemonList$.next(pokemonArray);
+        });
+      });
+  }
+
+  previousPokemon() {
+    if (this.pokeApiRepoSrv.offset > 0) {
+      this.pokeApiRepoSrv.offset -= 20;
+      this.fetchPokemonList();
+    }
+  }
+
+  nextPokemon() {
+    if (this.pokeApiRepoSrv.offset < 1320) {
+      this.pokeApiRepoSrv.offset += 20;
+      this.fetchPokemonList();
+    }
   }
 
   get pokemon() {
-    return this.pokemon$.asObservable();
-  }
-
-  initialPokemon() {
-    this.pokeSrv.getPokemonResponse(0).subscribe({
-      next: (data) => {
-        data.results.map((pokemon) => {
-          this.pokeSrv.getPokemonDetails(pokemon.url).subscribe({
-            next: (data: Pokemon) => {
-              this.pokemonList = [...this.pokemonList, data];
-            },
-          });
-        });
-      },
-    });
+    return this.pokemonList$.asObservable();
   }
 }
